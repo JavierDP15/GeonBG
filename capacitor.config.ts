8900fba38a67c6dcadab2db474d977c7d2100fdc
JavierDP15 <javierdp15@gmail.com>
2024-12-16 18:34:50 +0100

    Menu de inicio creado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'GeonBG',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+    },
+    ScreenOrientation: {
+      orientation: 'landscape' // Fijar a horizontal
+    }
+  }
 };
 
 export default config;
